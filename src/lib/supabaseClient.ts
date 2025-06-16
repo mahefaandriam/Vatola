a6e840319b9de975523b6fc09ccc2c88619ba7b8
Mahefa andriam <fenoandriams@gmail.com>
2025-06-16 12:03:49 +0300

    j

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-// It's best practice to store your keys in environment variables
-const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
