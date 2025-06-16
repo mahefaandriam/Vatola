cdd53c34b2ec8a3177b48aca5bc2c27c01340f3c
Mahefa andriam <fenoandriams@gmail.com>
2025-06-16 12:14:32 +0300

    j

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+export const supabase = createClient("https://xgowyqpigxvhpkjhatmt.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhnb3d5cXBpZ3h2aHBramhhdG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDczOTcsImV4cCI6MjA2NTU4MzM5N30.q2Uq5Y-J7VsHgDZ6AQBjLdqmOtUowqTy--YMA9QIloY")
